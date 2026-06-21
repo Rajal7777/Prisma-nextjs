8f0cbd97054b44cea2a00630e828713448f7e2e5
Rajal7777 <suwalrajal57@gmail.com>
2026-06-21 21:27:50 +0900

    added a navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/component/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,12 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <div className="min-h-screen bg-gray-500">
+          <Navbar />
+          <main className="flex w-full max-w-80rem">{children}</main>
+        </div>
+      </body>
     </html>
   );
 }
---
+++ auth/sigin/page.tsx
@@ -0,0 +1,9 @@
+const SignInPage = () => {
+    return ( 
+        <div className="min-h-[calc(100vh -10rem)] flex items-center justify-center">
+    <div className="max-w-full space-y-8"></div>
+        </div>
+     );
+}
+ 
+export default SignInPage;
